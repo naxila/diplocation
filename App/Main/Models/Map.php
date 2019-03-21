eafe7aac83a7097cf733f4d06840cea94d61d0db
naxila <al.pes@bk.ru>
2019-03-21 16:43:27 +0400

    Logs added

--- App/Main/Models/Map.php
+++ App/Main/Models/Map.php
@@ -4,9 +4,11 @@ class Map extends Model {
 
 	/* Work with Points */
 
-	public static function addPoint($deviceId, $title, $buildingId) {
+	public static function addPoint($deviceId, $title, $buildingId, $token) {
 
-		self::Insert("points", ["device_id" => $deviceId, "title" => $title, "building_id" => $buildingId]);
+		$admin = self::getAdminId($token);
+
+		self::Insert("points", ["device_id" => $deviceId, "title" => $title, "building_id" => $buildingId, "edited_by"=>$admin]);
 		$point = self::Query("SELECT * FROM points WHERE device_id='$deviceId'");
 		$point = mysqli_fetch_assoc($point);
 		if ($point != NULL){
@@ -16,8 +18,9 @@ class Map extends Model {
 		return false;
 	}
 
-	public static function updatePoint($id, $deviceId, $title, $buildingId) {
-		self::Update("points", ["device_id" => $deviceId, "title" => $title, "building_id" => $buildingId], ["id" => $id]);
+	public static function updatePoint($id, $deviceId, $title, $buildingId, $token) {
+		$admin = self::getAdminId($token);
+		self::Update("points", ["device_id" => $deviceId, "title" => $title, "building_id" => $buildingId, "edited_by"=>$admin], ["id" => $id]);
 		return true;
 	}
 
@@ -55,9 +58,10 @@ class Map extends Model {
 
 	/* Work with Vectors */
 
-	public static function addVector($startPoint, $endPoint, $distance, $direction) {
-
-		self::Insert("Vectors", ["start_point" => $start_point, "end_point" => $endPoint, "distance" => $distance, "direction" => $direction]);
+	public static function addVector($startPoint, $endPoint, $distance, $direction, $token) {
+		
+		$admin = self::getAdminId($token);
+		self::Insert("Vectors", ["start_point" => $start_point, "end_point" => $endPoint, "distance" => $distance, "direction" => $direction, "edited_by"=>$admin]);
 		$vector = self::Query("SELECT * FROM points WHERE start_point='$startPoint' AND end_point='$endPoint'");
 		$vector = mysqli_fetch_assoc($vector);
 		if ($vector != NULL){
@@ -67,14 +71,26 @@ class Map extends Model {
 		return false;
 	}
 
-	public static function updateVector($id, $startPoint, $endPoint, $distance, $direction) {
-		self::Update("Vectors", ["start_point" => $start_point, "end_point" => $endPoint, "distance" => $distance, "direction" => $direction], ["id" => $id]);
+	public static function updateVector($id, $startPoint, $endPoint, $distance, $direction, $token) {
+		$admin = self::getAdminId($token);
+		self::Update("Vectors", ["start_point" => $start_point, "end_point" => $endPoint, "distance" => $distance, "direction" => $direction, "edited_by"=>$admin], ["id" => $id]);
 		return true;
 	}
 
 	public static function deleteVector($id) {
 		self::Delete("Vectors", ["id" => $id]);
 		return true;
+	}
+
+	private function getAdminId($token) {
+		$admin = self::Query("SELECT * FROM access_tokens WHERE token='$token'");
+		$admin = mysqli_fetch_assoc($admin);
+
+		if ($admin) {
+			return $admin["admin_id"];
+		}
+
+		return 0;
 	}
 
 }
